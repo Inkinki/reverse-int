d2954537c4e22e46cff6248728a72f5000547295
Inki <inki.krik@gmail.com>
2022-02-02 23:55:46 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  numAbs = Math.abs(n);
+  const numToArray =  Array.from(String(numAbs), Number);
+  const reverseArray = numToArray.reverse();
+  const reverseNumber = Number(reverseArray.join(''));
+  return reverseNumber;
 }
+
+
+
